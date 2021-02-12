b7003dfdb85eafe180f191d5656c9ac4d1941122
Yordanos SoIomon <ybsolomon@dons.usfca.edu>
2021-02-12 13:26:49 -0800

    set up network request

---
+++ Flix/MovieCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieCell.swift
+//  Flix
+//
+//  Created by Yordanos on 2/11/21.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+}
